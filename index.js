b46528de366c4a09de37c19ff51d169316b2bf66
stackexpress-test <stackexpress-test@stackexpress.com>
2017-06-20 12:45:16 +0530

    Update index.js
    this is only a test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello NodeJS drie!')
+  response.send('Hello NodeJS drie! <BR> This is a test. <B><I>This is only a test.</I></B>')
 })
 
 app.listen(app.get('port'), function() {
